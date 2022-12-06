db367793089112888916312f74c7e0585743dbc2
iglis0n <117487700+iglis0n@users.noreply.github.com>
2022-12-06 13:56:08 -0300

    leitor

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Biblioteca;
 
 import java.io.BufferedReader;
@@ -5,12 +10,22 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        // TODO code application logic here
         Livro[] livros;
         Livro livro=new Livro();
         livros = livro.carregaLivros("livros.txt");
+        
+        Leitor[] leitores;
+        Leitor leitor=new Leitor(); 
+        leitores = leitor.carregaLeitores("leitores.txt");
+        
+        Fila[] filas;
+        Fila fila=new Fila(); 
+        filas = fila.carregaFila("fila.txt");
         
         Operacoes operacoes;
         operacoes = new Operacoes();
@@ -23,11 +38,16 @@ public class Main {
         for(;;){
             
             System.out.println("Escolha uma opção ");
-            System.out.println("1 - Listar todos os livros ");
-            System.out.println("2 - Listar todos livros por ordem de titulo ");
-            System.out.println("3 - Listar todos os livros por ordem de Autor ");
-            System.out.println("4 - Buscar todos os livros ");
-            System.out.println("0 - Fechar/Sair");
+            System.out.println("1 - Listar livros ");
+            System.out.println("2 - Listar livros por ordem de Titulo ");
+            System.out.println("3 - Listar livros por ordem de Autor ");
+            System.out.println("4 - Buscar livros ");
+            
+            System.out.println("5 - Buscar leitor");
+            System.out.println("6 - Listar leitores por ordem de nome-FAZER");
+            System.out.println("7 - Listar leitores por ordem de id-FAZER");
+
+            System.out.println("0 - Sair");
             opcao = teclado.nextLine();
 
             switch(opcao){
@@ -43,7 +63,9 @@ public class Main {
                 case "4":
                     operacoes.buscarLivros(livros);
                     //livro.buscar(livros); 
-                    break;   
+                case "5":
+                    operacoes.buscarLeitor(leitores);
+                    break; 
                 case "0":
                     System.out.println("FIM");
                     System.exit(0);
